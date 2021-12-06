f95428180037857d10d80e77922782b08237ce12
Bodhith <bodhith9@gmail.com>
2021-12-06 18:32:49 -0500

    level-1:first-call

--- publisher/app.js
+++ publisher/app.js
@@ -1,10 +1,18 @@
 const express = require("express")
+const request = require("request");
+const path = require('path');
 
 const app = express()
 const port = 5000
 
 app.get('/', function(req, res) {
-    res.sendFile("F:\\COLLEGE\\DS\\PROJECT 2\\publisher\\index.html")
+    res.sendFile(path.join(__dirname,"index.html"));
+});
+
+app.get('/getAllTopics', function(req, ress) {
+    request.get("http://Custom_API:4000/getAllTopics/pub", function(err, res, body) {   
+        ress.send(JSON.stringify(body), 200);
+    });
 });
 
 app.listen(port, () => {
