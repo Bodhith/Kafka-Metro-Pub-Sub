72e6d20dd026ec407d750c9a6da9d5c729de8da1
Bodhith <bodhith9@gmail.com>
2021-12-07 05:06:08 -0500

    level-1:completed

--- publisher/static/pubScript.js
+++ publisher/static/pubScript.js
@@ -5,17 +5,32 @@ function getAllTopics() {
         $("#stations").empty();
         for(topic of topics) {
             console.log("Topics", topic);
-            $("#stations").append("<option value="+topic[0]+">"+topic[1]+" --- [Station Code:"+topic[0]+"]</option>");
+            $("#stations").append(`<option value=${topic[0]}>${topic[1]} --- [Station Code: ${topic[0]}]</option>`);
         }
     });
 }
 
 function publish() {
-
+    $.post("/publish",
+        {
+            topicId: $("#stations").val(),
+            message: $("#message").val()
+        },
+        function(data) {
+            
+        }
+    );
 }
 
 function advertise() {
-
+    $.post("/advertise",
+        {
+            advertisement: $("#add").val(),
+        },
+        function(data) {
+            
+        }
+    );
 }
 
 function notify() {
--- subscriber/app.js
+++ subscriber/app.js
@@ -1,13 +1,37 @@
+const bodyParser = require('body-parser')
 const express = require("express")
+const request = require("request");
 const path = require('path');
 
 const app = express()
 const port = 5000
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname,"index.html"))
+app.use(express.static("static"))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+})); 
+
+app.get('/:userId', function(req, res) {
+    let userId = req.params.userId;
+    if(!userId) {
+        res.send(404);
+    }
+    res.sendFile(path.join(__dirname,"index.html"));
 });
 
+app.get('/getSubTopics/:userId', function(req, ress) {
+    let userId = req.params.userId;
+    if(!userId) {
+        res.send(404);
+    }
+    request.get(`http://Custom_API:4000/getSubTopics/${userId}`, function(err, res, body) {   
+        ress.send(JSON.parse(body), 200);
+    });
+});
+
+
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Subscriber Application listening at http://localhost:${port}`)
   })
